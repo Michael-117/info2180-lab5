5589679212a79fb2ada92d0af989a485cd4dc749
Michael Pearson <47512372+Michael-117@users.noreply.github.com>
2021-11-09 23:53:09 -0500

    Exercise 4

--- world.php
+++ world.php
@@ -11,20 +11,40 @@ if (empty($_GET['country']) && empty($_GET['context'])){
 }
 elseif (isset($_GET['country']) && empty($_GET['context'])){
   $query = "SELECT * FROM countries WHERE name like '%".$_GET['country']."%'";
-  echo $query;
   $stmt = $conn->query($query);
 }
 elseif (isset($_GET['country']) && isset($_GET['context'])){
   $query = "SELECT * FROM countries WHERE name like '%".$_GET['country']."%'";
-  echo $query;
   $stmt = $conn->query($query);
 }
 
 $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
 ?>
-<ul>
-<?php foreach ($results as $row): ?>
-  <li><?= $row['name'] . ' is ruled by ' . $row['head_of_state']; ?></li>
-<?php endforeach; ?>
+<table>
+  <tr>
+    <th>
+      Name
+    </th>
+    <th>
+      Continent
+    </th>
+    <th>
+      Independence
+    </th>
+    <th>
+      Head of State
+    </th>
+  </tr>
+  <?php foreach ($results as $row): ?>
+    <tr>
+      <td><?= $row['name']; ?></td>
+      <td><?= $row['continent']; ?></td>
+      <td><?= $row['independence_year']; ?></td>
+      <td><?= $row['head_of_state']; ?></td>
+    </tr>
+  <?php endforeach; ?>
+</table>
 </ul>
+
+<!-- SELECT cities.name, cities.district, cities.population FROM cities INNER JOIN countries ON cities.country_code = countries.code -->
